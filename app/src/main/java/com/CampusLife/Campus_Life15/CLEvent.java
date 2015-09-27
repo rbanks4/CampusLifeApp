17237c6b29ceddd5a66c71dd3f124effc5034aa7
rbanks4 <rbankskai@gmail.com>
2015-09-27 16:26:22 -0400

    Initial commit

---
+++ app/src/main/java/com/CampusLife/Campus_Life15/CLEvent.java
@@ -0,0 +1,124 @@
+package com.CampusLife.Campus_Life15;
+
+import android.widget.ViewFlipper;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class CLEvent {
+
+    private String prop0;
+    private String prop1;
+    private String prop2;
+    private String prop3;
+    //private String prop4;
+    private String prop5;
+    //private String mdate;
+    private ViewFlipper flip;
+    public int color;
+
+    DateFormat fday = new SimpleDateFormat("EEE MMM dd, yyyy");//this is how it should come out
+    DateFormat tday = new SimpleDateFormat("HH:mm:ss");
+    Date date;
+
+
+    public CLEvent(String prop0, String prop1, String prop2, String prop3, String prop5) {
+        this.prop0 = prop0;//date
+        try {
+            date = (Date) fday.parse(prop0);//now we should be able to sort
+            this.prop0 = fday.format(prop0);//update with the new date
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        this.prop1 = prop1;//title
+        if (prop2 != null) {
+            this.prop2 = prop2;//time
+        }
+        else{this.prop2 = "All Day";}
+        if (prop3 != null) {
+            this.prop3 = prop3;//location
+        }
+        else{this.prop3 = " ";}
+        //this.prop4 = prop4;//sponsor
+        if(prop5 != null) {
+            this.prop5 = prop5;//description
+        }
+        else{
+            this.prop5 = "No description available";
+        }
+        checkColor();
+    }
+    public CLEvent(String prop0, String prop1){//reserved for all day events
+        this.prop0 = prop0;//date
+        this.prop1 = prop5 = prop1;//title
+        prop2 = "All Day";
+        prop3 = " ";
+        checkColor();
+    }
+    public CLEvent(String prop0, String prop1, String prop5){//reserved for all day events
+        this.prop0 = prop0;//date
+        this.prop1 = prop1;//title
+        prop2 = "All Day";
+        prop3 = " ";
+        if (prop5 == null){
+            this.prop5 = "No description available.";
+        }
+        else {
+            this.prop5 = prop5;//desc
+        }
+        checkColor();
+    }
+    public CLEvent(String prop0, String prop1, String prop2, String prop5){//all day events with location
+        this.prop0 = prop0;//date
+        this.prop1 = prop1;//title
+        this.prop2 = prop2;//location
+        prop3 = "All Day";
+        if (prop5 == null){
+            this.prop5 = "No description available.";
+        }
+        else {
+            this.prop5 = prop5;//desc
+        }
+        checkColor();
+    }
+
+    private void checkColor(){
+        if (prop1.contains("No Classes") || prop1.contains("no classes")
+                || prop1.contains("No Class") || prop1.contains("no class")){//check the title name for "No Classes
+            color = 0xff000000;//black for no classes
+        }
+        else if ((prop2 == "All Day") || (prop3 == "All Day")){
+            color = 0xfff4821f;//orange for all day
+        }
+        else{
+            color = 0xff0e4c8b;
+        }
+    }
+
+    public String getProp0() { return prop0; }
+
+    public Date getDate() { return date; }
+
+    public String getProp1() {
+        return prop1;
+    }
+
+    public String getProp2() {
+        return prop2;
+    }
+
+    public String getProp3() {
+        return prop3;
+    }
+
+    //public String getProp4() { return prop4; }
+
+    public String getProp5() { return prop5; }
+
+    public ViewFlipper getFlip() { return flip; }
+
+    public void setFlip(ViewFlipper f){this.flip = f; }
+
+}
---
+++ app/src/main/java/com/CampusLife/Campus_Life15/ClCalendar.java
@@ -0,0 +1,354 @@
+package com.CampusLife.Campus_Life15;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.HashMap;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class ClCalendar extends Activity {
+
+    /*
+    We use Date and DateFormat in order to show the user the current date.
+    Cal limit is used to limit list of events we show.
+     */
+    Date date = new Date();
+    DateFormat day = new SimpleDateFormat("EEEE MMMM dd, yyyy  hh:mm:ss a");
+    //int calLimit = 30;
+    public String resString = " ";
+    String caltest;
+    private WebView mWebView;
+    String glURL = "https://www.google.com/calendar/feeds/dlc7torch%40gmail.com/private-7ee00fe08d8dd0be70fe658c2f363c7d/basic";
+    private TextView tview;
+    XMLParser cutty = new XMLParser();
+    Document caldoc;
+    private ProgressBar progress;
+    ArrayList<CLEvent> elist = new ArrayList<CLEvent>();
+    CalParser cparse = new CalParser();
+    CalendarAdapter adapter;
+    //private final int progr[]  = {30, 15, 20, 25, 20};
+    private int index;
+    ListView listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_cal2);
+
+        //mWebView = (WebView) findViewById(R.id.calxml);
+        //converting website into my own src code
+        tview = (TextView) findViewById(R.id.src);
+        progress = (ProgressBar)findViewById(R.id.progress);
+
+        // Enable Javascript
+        //WebSettings webSettings = mWebView.getSettings();
+        //webSettings.setJavaScriptEnabled(true);
+        //String glURL = "https://www.google.com/calendar/embed?src=dlc7torch%40gmail.com&ctz=America/New_York ";
+        //mWebView.loadUrl(glURL);
+        new GetXML().execute();
+        /*//build();
+
+        *//*
+        We make an action Bar to make sure we can go back to main,
+        setup the UI, and initialize default values
+         *//*
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        // Create a ScrollView to put your hero data in*/
+
+
+        //todo see if this works --------------------------------------it works!
+
+
+        //setContentView(R.layout.activity_calendar);
+        /*
+        We make an action Bar to make sure we can go back to main,
+        setup the UI, and initialize default values
+         */
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        LinearLayout activityLayout = new LinearLayout(this);
+        RelativeLayout mainLayout = new RelativeLayout(this);
+
+        adapter = new CalendarAdapter();
+        adapter.setContext(this);
+
+        RelativeLayout.LayoutParams ml = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.FILL_PARENT,
+                RelativeLayout.LayoutParams.FILL_PARENT);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        mainLayout.setLayoutParams(ml);
+        mainLayout.setVerticalScrollBarEnabled(true);
+
+        lp.weight = 1.0f;
+        activityLayout.setLayoutParams(lp);
+        activityLayout.setOrientation(LinearLayout.VERTICAL);
+        activityLayout.setPadding(16, 16, 16, 80);
+
+        ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        ViewGroup.LayoutParams flp = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        RelativeLayout.LayoutParams bot = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        bot.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+
+        //this listView layout is a placeholder, data will be put in it from code below
+        listview = new ListView(this);
+        listview.setLayoutParams(flp);
+        listview.setVerticalScrollBarEnabled(true);
+        //listview.setPadding(0, 0, 0, 60);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        //int bID = getResources().getIdentifier("button1", "id", getPackageName());
+        //View ViewButton = inflater.inflate(bID,null);
+        //listview.addFooterView(ViewButton);
+        activityLayout.addView(listview);
+
+        mainLayout.addView(activityLayout);
+
+        setContentView(mainLayout);
+
+
+    }
+    //we don't use this
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calendar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        onBackPressed();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        overridePendingTransition(R.anim.acceldecel, R.anim.acceldecelexit); //Reg: it works! Remember to apply the reverse
+
+        return super.onOptionsItemSelected(item);
+    }
+// TODO delete Cal() if it has no real use
+    public void Cal(){
+        //Reg: we use Intent to call a new activity
+        TextView textViewToChange = (TextView) findViewById(R.id.date);
+        textViewToChange.setText(day.format(date));
+    }
+
+    class GetXML extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+        //String resString = "wait on it...";
+        //TextView tview;
+
+
+        public GetXML() {
+            resString = "wait on it...";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            //setContentView(R.layout.activity_calendar);
+            //tview.setText(result);
+            //progress.setVisibility(View.GONE);
+            build(result);
+        }
+
+        @Override
+        protected String doInBackground(String... url) {
+            try {
+
+
+
+                Thread.sleep(4000);
+
+                URL google = new URL(glURL);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) google.openConnection();
+
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) // Read line by line
+                    sb.append(line + "\n");
+
+                resString = sb.toString(); // Result is here
+
+                is.close(); // Close the stream
+                urlConnection.disconnect();
+
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return resString;
+        }
+
+
+
+    }
+
+    public void build(String res){
+        //create a document to find all of the elements
+        caldoc = cutty.getDomElement(res);//makes the document
+        caldoc.getDocumentElement().normalize();
+        NodeList nList = caldoc.getElementsByTagName("entry");//makes a list of nodes
+        caltest = " ";//now that we have our doc, we can clear it
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {//todo split data into bite sized events
+            Node nNode = nList.item(temp);
+            caltest.concat("\nCurrent Element :"
+                    + nNode.getNodeName());
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {//logic returning somthing else
+                Element eElement = (Element) nNode;
+                    /*resString.concat("Title : "
+                            + eElement.getAttribute("rollno"));*/
+                String Title = (eElement.getElementsByTagName("title").item(0).getTextContent()
+                        +"\n");
+
+                //Log.w("myApp", caltest);
+                String Summary = (eElement.getElementsByTagName("summary").item(0).getTextContent()
+                        +"\n");
+                //List<String> desc = new ArrayList<String>(cparse.fixSummary(Summary));
+                String[] changes = cparse.fixSummary(Summary);
+                //desc.toArray(changes);
+
+                CLEvent event = new CLEvent(changes[0], Title, changes[1], changes[2], changes[3]);
+                //Log.w("myApp", "event with 5 elements: " + changes[0] + Title + changes[1] + changes[2] + changes[3]);
+                elist.add(event);
+
+            }
+
+        }
+
+
+
+        //resString=cutty.getValue(caldoc.getElement,"title");
+        // TODO Auto-generated method stub
+        //onPostExecute(resString);
+        //progress.setVisibility(View.GONE);
+        //Collections.sort(elist);
+        Collections.sort(elist, new DateComparator());
+        adapter.setObjects(elist);
+        listview.setAdapter(adapter);
+        //tview.setText(caltest);
+        //return resString;
+    }
+
+
+    //todo sort all of the objects we parsed by date
+    public static <CLEvent> void sort(List<CLEvent> list, Comparator<? super CLEvent> c){
+        //don't know how to use it
+    }
+
+    //todo if this works, delete the other sort function
+    public class DateComparator implements Comparator<CLEvent> {
+        public int compare(CLEvent o1, CLEvent o2) {
+            if (o1.getDate().before(o2.getDate())) {
+                return -1;
+            } else if (o1.getDate().after(o2.getDate())) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+
+
+
+}
