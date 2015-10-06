e30789134f5d647654f614fedbcdfe9bdb8f2073
rbanks4 <rbankskai@gmail.com>
2015-10-05 22:59:44 -0400

    Changed alot of things: -Updated the Calendar UI   +Added a splash screen for loading   +Instead of those blocks, we now have a list view I can stomach   +Dates are easier to read -Updated the Contacts UI   +Pictures are smaller and text is bigger   +Divider exist between the title and description   +General info added to the bottom: Address, Office Hours...etc. -Updated Info UI   +Sends you right to the website in mobile view   +Removed the redundancy of the fragmented activity   +Removing the fragments also resolved the crashing issue...after I fixed the fragment crashing issue... :( ...(talk about redundant, haha) -Calendar date issue resolved   +Days are now listing upcoming events and not outdated ones   +Calendar is now sorted (IN ORDER) for sure this time   +Did a few things with the xml parser and CLEvent class so that dates show up cleaner (for example, 2:30 PM won't show up as 02:30 PM)

--- app/src/main/java/com/CampusLife/Campus_Life15/ClCalendar.java
+++ app/src/main/java/com/CampusLife/Campus_Life15/ClCalendar.java
@@ -3,20 +3,26 @@ package com.CampusLife.Campus_Life15;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -32,11 +38,13 @@ import java.io.StringReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.text.DateFormat;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.w3c.dom.Document;
@@ -57,8 +65,11 @@ public class ClCalendar extends Activity {
     Cal limit is used to limit list of events we show.
      */
     Date date = new Date();
+
     DateFormat day = new SimpleDateFormat("EEEE MMMM dd, yyyy  hh:mm:ss a");
+    ImageView logo;
     //int calLimit = 30;
+    LinearLayout loadingLayout;
     public String resString = " ";
     String caltest;
     private WebView mWebView;
@@ -73,39 +84,19 @@ public class ClCalendar extends Activity {
     //private final int progr[]  = {30, 15, 20, 25, 20};
     private int index;
     ListView listview;
+    String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_calendar);
         setContentView(R.layout.activity_cal2);
 
-        //mWebView = (WebView) findViewById(R.id.calxml);
-        //converting website into my own src code
-        tview = (TextView) findViewById(R.id.src);
-        progress = (ProgressBar)findViewById(R.id.progress);
+        //tview = (TextView) findViewById(R.id.src);//use this to display our progress
 
-        // Enable Javascript
-        //WebSettings webSettings = mWebView.getSettings();
-        //webSettings.setJavaScriptEnabled(true);
-        //String glURL = "https://www.google.com/calendar/embed?src=dlc7torch%40gmail.com&ctz=America/New_York ";
-        //mWebView.loadUrl(glURL);
-        new GetXML().execute();
-        /*//build();
-
-        *//*
-        We make an action Bar to make sure we can go back to main,
-        setup the UI, and initialize default values
-         *//*
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        // Create a ScrollView to put your hero data in*/
-
+        //new GetXML().execute();
 
         //todo see if this works --------------------------------------it works!
 
-
-        //setContentView(R.layout.activity_calendar);
         /*
         We make an action Bar to make sure we can go back to main,
         setup the UI, and initialize default values
@@ -115,6 +106,7 @@ public class ClCalendar extends Activity {
 
         LinearLayout activityLayout = new LinearLayout(this);
         RelativeLayout mainLayout = new RelativeLayout(this);
+        loadingLayout = new LinearLayout(this);
 
         adapter = new CalendarAdapter();
         adapter.setContext(this);
@@ -125,6 +117,11 @@ public class ClCalendar extends Activity {
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout.LayoutParams ll = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.FILL_PARENT);
+        ll.gravity = Gravity.CENTER_HORIZONTAL;
+        ll.gravity = Gravity.CENTER_VERTICAL;
 
         mainLayout.setLayoutParams(ml);
         mainLayout.setVerticalScrollBarEnabled(true);
@@ -132,7 +129,12 @@ public class ClCalendar extends Activity {
         lp.weight = 1.0f;
         activityLayout.setLayoutParams(lp);
         activityLayout.setOrientation(LinearLayout.VERTICAL);
-        activityLayout.setPadding(16, 16, 16, 80);
+        activityLayout.setPadding(16, 16, 16, 16);//padding should be equal
+
+        loadingLayout.setLayoutParams(ll);
+        loadingLayout.setOrientation(LinearLayout.VERTICAL);
+        loadingLayout.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        loadingLayout.setBackgroundColor(0xff0e4c8b); //Clayton Blue
 
         ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -147,23 +149,44 @@ public class ClCalendar extends Activity {
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
         bot.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 
-
+        //todo find a way to change the color of the circle to indicate months
+        /*View circle = (View) findViewById(R.id.circle);
+        ShapeDrawable btm = new ShapeDrawable();
+        btm = (ShapeDrawable) circle.getBackground();
+        //btm.set*/
+        //todo give the user the ability to subscribe an event to their calendar
         //this listView layout is a placeholder, data will be put in it from code below
         listview = new ListView(this);
         listview.setLayoutParams(flp);
         listview.setVerticalScrollBarEnabled(true);
         //listview.setPadding(0, 0, 0, 60);
-        LayoutInflater inflater = LayoutInflater.from(this);
-        //int bID = getResources().getIdentifier("button1", "id", getPackageName());
-        //View ViewButton = inflater.inflate(bID,null);
-        //listview.addFooterView(ViewButton);
+
+        //this is the loading splash screen
+        logo = new ImageView(this);
+        logo.setImageResource(R.drawable.campuslife24);
+        progress = new ProgressBar(this);
+        //tview = new TextView(this);
+        //tview.setText("Loading...");
+        //tview.setPadding(8, 8, 8, 8);
+        //tview.setTextColor(0xffffffff);//basic white
+        //Animation loadingmovement = AnimationUtils.loadAnimation(this,R.anim.backacceldecel);
+        //tview.setAnimation(loadingmovement);
+        //tview.startAnimation(loadingmovement);
+        loadingLayout.addView(logo);
+        loadingLayout.addView(progress);
+        //loadingLayout.addView(tview);
+        mainLayout.addView(loadingLayout);
+        //setContentView(loadingLayout);//loading screen for now
+        new GetXML().execute();
+
         activityLayout.addView(listview);
 
         mainLayout.addView(activityLayout);
 
+        //mainLayout.setBackgroundColor(0xFF59B3DB);//that sky blue
+        //activityLayout.setBackgroundColor(0xFF59B3DB);//that sky blue
+        //todo put the second content view somwhere after the background computation is done
         setContentView(mainLayout);
-
-
     }
     //we don't use this
     private class StableArrayAdapter extends ArrayAdapter<String> {
@@ -197,6 +220,10 @@ public class ClCalendar extends Activity {
         getMenuInflater().inflate(R.menu.menu_calendar, menu);
         return true;
     }
+    /*
+    this will show as the data for the calendar is loading
+    */
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -210,15 +237,9 @@ public class ClCalendar extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-        overridePendingTransition(R.anim.acceldecel, R.anim.acceldecelexit); //Reg: it works! Remember to apply the reverse
+        overridePendingTransition(R.anim.backacceldecelexit,R.anim.backacceldecel); //Reg: it works! Remember to apply the reverse
 
         return super.onOptionsItemSelected(item);
-    }
-// TODO delete Cal() if it has no real use
-    public void Cal(){
-        //Reg: we use Intent to call a new activity
-        TextView textViewToChange = (TextView) findViewById(R.id.date);
-        textViewToChange.setText(day.format(date));
     }
 
     class GetXML extends AsyncTask<String, Void, String> {
@@ -235,7 +256,10 @@ public class ClCalendar extends Activity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-
+            //tview.setVisibility(View.GONE);
+            progress.setVisibility(View.GONE);
+            logo.setVisibility(View.GONE);
+            loadingLayout.setVisibility(View.GONE);
             //setContentView(R.layout.activity_calendar);
             //tview.setText(result);
             //progress.setVisibility(View.GONE);
@@ -245,9 +269,7 @@ public class ClCalendar extends Activity {
         @Override
         protected String doInBackground(String... url) {
             try {
-
-
-
+                //show the status that it's loading
                 Thread.sleep(4000);
 
                 URL google = new URL(glURL);
@@ -269,17 +291,11 @@ public class ClCalendar extends Activity {
 
                 e.printStackTrace();
 
-
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-
             return resString;
         }
-
-
-
     }
 
     public void build(String res){
@@ -289,53 +305,57 @@ public class ClCalendar extends Activity {
         NodeList nList = caldoc.getElementsByTagName("entry");//makes a list of nodes
         caltest = " ";//now that we have our doc, we can clear it
 
-        for (int temp = 0; temp < nList.getLength(); temp++) {//todo split data into bite sized events
+        for (int temp = 0; temp < nList.getLength(); temp++) {
             Node nNode = nList.item(temp);
             caltest.concat("\nCurrent Element :"
                     + nNode.getNodeName());
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {//logic returning somthing else
                 Element eElement = (Element) nNode;
-                    /*resString.concat("Title : "
-                            + eElement.getAttribute("rollno"));*/
-                String Title = (eElement.getElementsByTagName("title").item(0).getTextContent()
-                        +"\n");
-
-                //Log.w("myApp", caltest);
-                String Summary = (eElement.getElementsByTagName("summary").item(0).getTextContent()
-                        +"\n");
-                //List<String> desc = new ArrayList<String>(cparse.fixSummary(Summary));
+                //gets event title
+                String Title = (eElement.getElementsByTagName("title").item(0).getTextContent());
+                //fixes quoting issue
+                if (Title.contains("&quot;")){Title.replaceAll("&quot;","");}
+                //gets event summary
+                String Summary = (eElement.getElementsByTagName("summary").item(0).getTextContent());
+                //CalParserclass parses the rest of the data in this event neatly
                 String[] changes = cparse.fixSummary(Summary);
-                //desc.toArray(changes);
-
+                //events are spit out here in order (date, title, time, location, desc)
                 CLEvent event = new CLEvent(changes[0], Title, changes[1], changes[2], changes[3]);
-                //Log.w("myApp", "event with 5 elements: " + changes[0] + Title + changes[1] + changes[2] + changes[3]);
+                //add the event to our event list
                 elist.add(event);
 
             }
 
         }
-
-
-
-        //resString=cutty.getValue(caldoc.getElement,"title");
-        // TODO Auto-generated method stub
-        //onPostExecute(resString);
-        //progress.setVisibility(View.GONE);
-        //Collections.sort(elist);
+        //sort the events in order
         Collections.sort(elist, new DateComparator());
+        //delete old events
+        elist = removeOldDates(elist);
+
+        //adds event list to adapter
         adapter.setObjects(elist);
+        //makes a list view out of the adapter
+        //todo: make animations for appearance of list view
         listview.setAdapter(adapter);
-        //tview.setText(caltest);
-        //return resString;
+
     }
 
+    public ArrayList<CLEvent> removeOldDates(ArrayList<CLEvent> object){
+        ArrayList<CLEvent> myList = new ArrayList<>();
+        Iterator<CLEvent> event = object.iterator();
+         while(event.hasNext()) {
+             CLEvent user = event.next();
+            if (user.getDate().before(date)) {//if event is past current date
+                event.remove(); //remove the event
+            }
+             else{
+                myList.add(user); //add to ArrayList
+            }
 
-    //todo sort all of the objects we parsed by date
-    public static <CLEvent> void sort(List<CLEvent> list, Comparator<? super CLEvent> c){
-        //don't know how to use it
+        }
+        return myList;
     }
 
-    //todo if this works, delete the other sort function
     public class DateComparator implements Comparator<CLEvent> {
         public int compare(CLEvent o1, CLEvent o2) {
             if (o1.getDate().before(o2.getDate())) {
@@ -347,8 +367,4 @@ public class ClCalendar extends Activity {
             }
         }
     }
-
-
-
-
 }
